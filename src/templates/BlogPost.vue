06e6566e8ebf84361a3b8cf428edabcb4e44a1b7
retrorocket <mail@retrorocket.biz>
2020-10-27 16:26:50 +0900

    adsのjsを追加

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -296,6 +296,12 @@ export default {
         },
       ],
       title: this.decNumRefToString(this.$page.blogPost.title),
+      script: [
+        {
+          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+          async: true,
+        },
+      ],
     };
   },
 };
